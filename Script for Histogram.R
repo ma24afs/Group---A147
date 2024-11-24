ea34cd4906f569abe13440d30cfa4e7f1929e173
Aman Aman <za24aci@herts.ac.uk>
2024-11-24 21:24:04 +0000

    added code for Spearman Rho test

--- Script for Histogram.R
+++ Script for Histogram.R
@@ -1,15 +1,19 @@
-# Load necessary library
+
 library(ggplot2)
 
-# Load your data
-data <- read.csv("C:/Users/haroo/OneDrive/TR&D/Group---A147/flight_data.csv")
+# read csv
+data <- read.csv("C:/Users/Asus/Documents/Group project/Group---A147/flight_data.csv")
 
-# Create histogram with frequency and overlay bell curve
+# histrogram 
 ggplot(data, aes(x = Price)) +
   geom_histogram(aes(y = ..count..), bins = 20, fill = "skyblue", color = "black", alpha = 0.7) +
   stat_function(fun = function(x) {
     dnorm(x, mean = mean(data$Price, na.rm = TRUE), sd = sd(data$Price, na.rm = TRUE)) *
       nrow(data) * diff(range(data$Price)) / 20
   }, color = "blue", size = 1) +
-  labs(title = "Histogram of Flight Prices (Frequency) with Bell Curve", x = "Price", y = "Frequency") +
+  labs(title = "Histogram for Price", x = "Price", y = "Frequency") +
   theme_minimal()
+
+# Spearman Rho
+spearman_rho <- cor(data$Duration, data$Price, method = "spearman")
+print(spearman_rho)
